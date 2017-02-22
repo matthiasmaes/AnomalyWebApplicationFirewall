cd2e0d2e28c28e9345468e8d429409d96299e420
Matthias Maes <matthias.maes@outlook.com>
2017-02-22 15:15:34 +0100

    Fixed issue: extra record object Added: lines/second

--- Profiler/profiler.py
+++ Profiler/profiler.py
@@ -1,16 +1,11 @@
 # Public classes
-import re
-import string
 import json
-import sys
 import progressbar
 import datetime
-import os
 import threading
 from pymongo import MongoClient
 from optparse import OptionParser
 
-# Private classes
 from record import Record
 from connection import Connection
 
@@ -18,8 +13,7 @@ from connection import Connection
 #### Init global vars ####
 initTime = str(datetime.datetime.now().hour) + "_" +  str(datetime.datetime.now().minute) + "_" +  str(datetime.datetime.now().second)
 startTime = datetime.datetime.now()
-converted = 0
-activeWorkers = 0
+converted, activeWorkers = 0, 0
 ##########################
 
 
@@ -28,11 +22,9 @@ parser = OptionParser()
 parser.add_option("-p", "--ping", action="store_true", dest="ping", default=False, help="Try to resolve originating domains to ip for geolocation")
 parser.add_option("-b", "--bot", action="store_true", dest="bot", default=False, help="Filter search engine bots")
 parser.add_option("-d", "--debug", action="store_true", dest="debug", default=False, help="Show debug messages")
-parser.add_option("-l", "--log", action="store", dest="log", default="input/FormattedBigLog.min.txt", help="Input log file for profiler")
 parser.add_option("-t", "--threads", action="store", dest="threads", default="2", help="Amout of threats that can be used")
 parser.add_option("-x", "--lines", action="store", dest="linesPerThread", default="100", help="Max lines per thread")
 parser.add_option("-m", "--mongo", action="store", dest="outputMongo", default="test", help="Input via mongo")
-parser.add_option
 options, args = parser.parse_args()
 ######################
 
@@ -72,25 +64,18 @@ bar.start()
 def processLine(lines, index):
 	for inputLine in lines:
 
-
-
-		newRecord = Record(inputLine['method'], inputLine['url'], inputLine['code'], inputLine['size'])
-
+		
 
 		connectionTime = (inputLine['timestamp'].replace('[', '').replace('/', ':').split(':'))[3]
 
-
-
 		#### Add document on first occurance  ####
-		if MongoDB.find({"url": newRecord.getURL()}).count() == 0:
-			MongoDB.insert_one(newRecord.__dict__)
+		if MongoDB.find({"url": inputLine['url'] }).count() == 0:
+			MongoDB.insert_one((Record(inputLine['method'], inputLine['url'], inputLine['code'], inputLine['size'])).__dict__)
 		##########################################
 
 		#### Add Connection to db ####
 
-		userAgent, accessedBy = '', ''
-
-
+		accessedBy = ''
 
 		if options.bot:
 			if next((True for bot in bots if inputLine['uagent'] in bot), False):
@@ -100,7 +85,7 @@ def processLine(lines, index):
 		else:
 			accessedBy = 'Bot filtering disabled use: --bot'
 
-		MongoDB.update({"url": newRecord.getURL()}, {'$push': {'connection': Connection(inputLine['ip'], connectionTime, options.ping, accessedBy, inputLine['requestUrl']).__dict__}})
+		MongoDB.update({"url": inputLine['url'] }, {'$push': {'connection': Connection(inputLine['ip'], connectionTime, options.ping, accessedBy, inputLine['requestUrl']).__dict__}})
 
 		##############################
 
@@ -122,38 +107,39 @@ def processLine(lines, index):
 		print "[DEBUG] Active workers: {}".format(activeWorkers)
 		print "[DEBUG] Lines processed: {}".format(index)
 		print '[DEBUG] Lines / seconds: {}'.format(index / ((datetime.datetime.now() - startTime).total_seconds()))
+
 		
 
 
-
+###########################################
+#### TODO: USE RANGES INSTEAD OF LISTS ####
+###########################################
 
 #### Prepare workload and send to worker ####
 threads, progress, lines = [], [], list()
 
-with open(options.log) as fileobject:
-	for index, line in enumerate(InputMongoDB.find(), 1):
+for index, line in enumerate(InputMongoDB.find(), 1):
 
-		lines.append(line)
+	lines.append(line)
 
-		if index == num_lines or index % int(float(options.linesPerThread)) == 0 or index % int(options.linesPerThread) == 0:					
-			
+	if index == num_lines or index % int(float(options.linesPerThread)) == 0 or index % int(options.linesPerThread) == 0:					
+		
 
-			#### Hold until worker is free ####
-			while str(activeWorkers) == str(options.threads):
-				pass
-			###################################
-
-
-			#### Start of worker ####
-			activeWorkers += 1
-			t = threading.Thread(target=processLine, args=(lines,index,))
-			threads.append(t)
-			t.start()
-			#########################
+		#### Hold until worker is free ####
+		while str(activeWorkers) == str(options.threads):
+			pass
+		###################################
 
 
-			lines = list()
+		#### Start of worker ####
+		activeWorkers += 1
+		t = threading.Thread(target=processLine, args=(lines,index,))
+		threads.append(t)
+		t.start()
+		#########################
 
+
+		lines = list()
 ############################################
 
 
@@ -166,5 +152,6 @@ bar.finish()
 
 #### Print statistics ####
 print("Total execution time: {} seconds".format((datetime.datetime.now() - startTime).total_seconds()))
+print("Average lines per second: {} l/s".format(int(num_lines / (datetime.datetime.now() - startTime).total_seconds())))
 # TODO: More statistics
 ##########################
