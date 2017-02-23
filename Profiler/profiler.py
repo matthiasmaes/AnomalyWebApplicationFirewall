14ef1ba7513403a9fda31af5ad745596e04ca7d2
Matthias Maes <matthias.maes@outlook.com>
2017-02-23 09:16:14 +0100

    Weekday sorting

--- Profiler/profiler.py
+++ Profiler/profiler.py
@@ -4,6 +4,7 @@ import progressbar
 import datetime
 import sys
 import threading
+import calendar
 from pymongo import MongoClient
 from optparse import OptionParser
 
@@ -26,7 +27,6 @@ parser.add_option("-d", "--debug", action="store_true", dest="debug", default=Fa
 parser.add_option("-t", "--threads", action="store", dest="threads", default="8", help="Amout of threats that can be used")
 parser.add_option("-x", "--lines", action="store", dest="linesPerThread", default="250", help="Max lines per thread")
 parser.add_option("-m", "--mongo", action="store", dest="inputMongo", default="testCase", help="Input via mongo")
-parser.add_option("-s", "--stop", action="store", dest="stopIndex", default="500", help="Only process certain amount of records")
 options, args = parser.parse_args()
 ######################
 
@@ -67,8 +67,9 @@ def processLine(records, index):
 	
 	for inputLine in records:
 
-		
-		connectionTime = (inputLine['timestamp'].replace('[', '').replace('/', ':').split(':'))[3]
+		splittedTime = (inputLine['timestamp'].replace('[', '').replace('/', ':').split(':'))
+		connectionTime = splittedTime[3]
+		connectionDay = (datetime.datetime(int(splittedTime[2]), int(list(calendar.month_abbr).index(splittedTime[1])), int(splittedTime[0]))).weekday()
 
 		#### Add document on first occurance  ####
 		if OutputMongoDB.find({"url": inputLine['url'] }).count() == 0:
@@ -87,7 +88,7 @@ def processLine(records, index):
 		else:
 			accessedBy = 'Bot filtering disabled use: --bot'
 
-		OutputMongoDB.update({"url": inputLine['url'] }, {'$push': {'connection': Connection(inputLine['ip'], connectionTime, options.ping, accessedBy, inputLine['requestUrl']).__dict__}})
+		OutputMongoDB.update({"url": inputLine['url'] }, {'$push': {'connection': Connection(inputLine['ip'], connectionTime, connectionDay, options.ping, accessedBy, inputLine['requestUrl']).__dict__}})
 
 		##############################
 
@@ -111,12 +112,6 @@ def processLine(records, index):
 		print '[DEBUG] Lines / seconds: {}'.format(index / ((datetime.datetime.now() - startTime).total_seconds()))
 
 		
-
-
-###########################################
-#### TODO: USE RANGES INSTEAD OF LISTS ####
-###########################################
-
 #### Prepare workload and send to worker ####
 threads, progress = [], []
 startRange = 0
@@ -126,13 +121,7 @@ results = InputMongoDB.find()
 
 for index, line in enumerate(results, 1):
 
-
-	if index == int(options.stopIndex):
-		sys.exit()
-
-	if index == num_lines or index % int(options.linesPerThread) == 0:
-
-		
+	if index == num_lines or index % int(options.linesPerThread):
 
 		#### Hold until worker is free ####
 		while str(activeWorkers) == str(options.threads):
