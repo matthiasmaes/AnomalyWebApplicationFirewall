5e6c91f411d342c7399d82dd75e8d62cf965bb8b
Matthias Maes <matthias.maes@outlook.com>
2017-03-28 12:10:21 +0200

    [Docs] Test read and remove, failed: data loss

--- Test/testHash.py
+++ Test/testHash.py
@@ -5,8 +5,8 @@ import hashlib
 def md5(fname):
 	hash_md5 = hashlib.md5()
 	with open(fname, "rb") as f:
-		f.seek(-4096, 2)
-		hash_md5.update(f.read(4096))
+		f.seek(-500, 2)
+		hash_md5.update(f.read(500))
 
 	return hash_md5.hexdigest()
 
