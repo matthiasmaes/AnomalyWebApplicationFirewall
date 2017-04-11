9f03ca44155864128ea317eadd328123f81f8568
Matthias Maes <matthias.maes@outlook.com>
2017-04-11 13:04:34 +0200

    [Maintenance] Remove smelly code

--- 0. Helper/logGenerator.py
+++ 0. Helper/logGenerator.py
@@ -1,6 +1,6 @@
 import random
 
-with open ('access.log', 'w+') as file:
+with open ('access.log', 'w+') as fileIO:
 
 	for x in xrange(0,500):
 		day = str(random.randint(1, 30))
@@ -14,6 +14,6 @@ with open ('access.log', 'w+') as file:
 		minute = '0' + minute if int(minute) < 10 else minute
 		second = '0' + second if int(second) < 10 else second
 
-		file.write('::1 - - [' + day + '/Apr/2017:' + hour + ':' + minute + ':' + second + ' +0200] "GET /test/' +  url + '.html" 200 202575 "http://localhost:8080/test/' +  random.choice(['index', 'contact', 'about']) + '.html" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"\n')
+		fileIO.write('::1 - - [' + day + '/Apr/2017:' + hour + ':' + minute + ':' + second + ' +0200] "GET /test/' +  url + '.html" 200 202575 "http://localhost:8080/test/' +  random.choice(['index', 'contact', 'about']) + '.html" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"\n')
 
-		file.write('::1 - - [' + day + '/Apr/2017:' + hour + ':' + minute + ':' + second + ' +0200] "GET /favicon.ico HTTP/1.1" 200 202575 "http://localhost:8080/test/' +  url + '.html" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"\n')
+		fileIO.write('::1 - - [' + day + '/Apr/2017:' + hour + ':' + minute + ':' + second + ' +0200] "GET /favicon.ico HTTP/1.1" 200 202575 "http://localhost:8080/test/' +  url + '.html" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"\n')
