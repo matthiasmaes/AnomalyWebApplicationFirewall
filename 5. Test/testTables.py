8262515af940f183333a95afdde01105326131b8
Matthias Maes <matthias.maes@outlook.com>
2017-04-12 15:10:58 +0200

    [Maintenance] Small mod to test

--- 5. Test/testTables.py
+++ 5. Test/testTables.py
@@ -1,12 +1,11 @@
 import iptc
 from datetime import datetime
-# CREATE NEW TABLE #
-table = iptc.Table(iptc.Table.FILTER)
 
-chain = iptc.Chain(table, "INPUT")
-
-rule = iptc.Rule()
-rule.src = "192.168.137.129" 
-rule.target = rule.create_target("DROP")
-rule.match = rule.create_match("comment").comment = str(datetime.now())
-chain.insert_rule(rule)
+def blockIpTable(ip):
+	table = iptc.Table(iptc.Table.FILTER)
+	chain = iptc.Chain(table, "INPUT")
+	rule = iptc.Rule()
+	rule.src = ip
+	rule.target = rule.create_target("DROP")
+	rule.match = rule.create_match("comment").comment = str(datetime.now())
+	chain.insert_rule(rule)
