fe1d39b329bf119e73ea16e4af4d11a4af55a89d
Matthias Maes <matthias.maes@outlook.com>
2017-04-20 13:41:49 +0200

    [Maintenance] Move and rename iptablesAnalyser.py

---
+++ 3. Firewall/iptablesAnalyser.py
@@ -0,0 +1,17 @@
+import iptc
+from datetime import datetime
+
+table = iptc.Table(iptc.Table.FILTER)
+chain = iptc.Chain(table, "INPUT")
+index = 0
+
+for rule in chain.rules:
+	index += 1
+	print index
+	for match in rule.matches:
+		if match.comment != None:
+			datetime_object = datetime.strptime(match.comment, "%y-%m-%d %H:%M:%S")
+
+		if datetime.now() > datetime_object:
+			chain.delete_rule(rule)
+			print "Rule successfully deleted"
